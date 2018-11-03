e93391255f39e35ad8a60ccac1e971282eb5890d
mediumwb <medium4uk@gmail.com>
2018-11-03 21:22:00 +0200

    v1.1

--- js/main.js
+++ js/main.js
@@ -1,39 +1,60 @@
 $( document ).ready(function() {
     var pulling = false,
         pullObject = $('.square');
+    var mouseX, mouseY;
     
-    pullObject.mousedown(function(){
+    pullObject.mousedown(function(e){
         pulling = true;
-        $('.main').mousemove(function(event){
-            var x = event.pageX,
-                y = event.pageY;
-            if(pulling){
-                var pullHeight = pullObject.height(),
-                    pullWidth = pullObject.width(),
-                    pullObjectPosition = pullObject.position();
-                //positioning
-                pullObject.offset({top: y-(pullHeight)/2, left: x-(pullWidth/2)});
-                //x,y center of drugObject
-                var screenPosition = [pullObjectPosition.left + pullWidth/2, pullObjectPosition.top + pullHeight/2];
-                //drugObject background
-                if(screenPosition[0] < $(window).width()/2){
-                    if(screenPosition[1] < $(window).height()/2){
-                        pullObject.css("background-color","#000");
-                    }
-                    else {
-                        pullObject.css("background-color","darkorange");
-                    }
-                }
-                else if(screenPosition[1] < $(window).height()/2){
-                    pullObject.css("background-color","greenyellow");
+        mouseX= e.pageX;
+        mouseY= e.pageY;
+    }); 
+    $('.main').mousemove(function(e){
+        if(pulling){
+            var x = mouseX - e.pageX,
+                y = mouseY - e.pageY;
+
+            var pullHeight = pullObject.height(),
+                pullWidth = pullObject.width(),
+                pullObjectPosition = pullObject.position();
+            
+         
+            //positioning v1.0
+            //pullObject.offset({top: y-(pullHeight)/2, left: x-(pullWidth/2)});
+            //positioning v1.1
+            pullObjectPosition.left -= x;
+            pullObjectPosition.top -= y;
+            
+            if (pullObjectPosition.left>0 && 
+                pullObjectPosition.left < $(window).width() - pullHeight && 
+                pullObjectPosition.top>0 && 
+                pullObjectPosition.top < $(window).height() - pullWidth) {
+                pullObject.offset({top: pullObjectPosition.top, left: pullObjectPosition.left});
+            }
+            
+            mouseX= e.pageX,
+            mouseY= e.pageY;
+                
+            console.log("top: " + pullObjectPosition.top + " left: " + pullObjectPosition.left);
+            //x,y center of drugObject
+            var screenPosition = [pullObjectPosition.left + pullWidth/2, pullObjectPosition.top + pullHeight/2];
+            //drugObject background against window position
+            if(screenPosition[0] < $(window).width()/2){
+                if(screenPosition[1] < $(window).height()/2){
+                    pullObject.css("background-color","#000");
                 }
                 else {
-                    pullObject.css("background-color","darkblue");
+                    pullObject.css("background-color","darkorange");
                 }
-            };
-        });
-    });
-    pullObject.mouseup(function(){
+            }
+            else if(screenPosition[1] < $(window).height()/2){
+                pullObject.css("background-color","greenyellow");
+            }
+            else {
+                pullObject.css("background-color","darkblue");
+            }
+            }});
+    $('.main').mouseup(function(){
         pulling = false;
-    });
+    }).mouseleave(function(){
+        pulling = false;});
 });
